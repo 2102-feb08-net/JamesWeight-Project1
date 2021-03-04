49dde407ce1755df69629b3f27643218d02feb70
JamesWeight <JamesWeight>
2021-03-04 00:20:45 -0800

    Start2021mar4am1220

---
+++ DB/Controllers/Library.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Customer : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<UI.Domain.Customer> _logger;
+
+        public Customer(ILogger<UI.Domain.Customer> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<UI.Domain.Customer> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new UI.Domain.Customer
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
